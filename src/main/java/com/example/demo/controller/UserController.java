c39af9ba85c9feacf2e6219b625082eaac1198d3
ibmpariwesh <ibmpariwesh@gmail.com>
2022-01-20 15:39:45 +0530

    validations implemented

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,17 @@
-package com.example.demo;
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
@@ -16,13 +23,18 @@ public class UserController {
 	IUserService userService;
 
 	@GetMapping("/user")
-	String getUser() {
-		System.out.println("testing");
-		return "testing";
+	Iterable<User> getUsers() {
+		return userService.getUsers();
+	}
+	
+	@GetMapping("/user/{id}")
+	Optional<User> getUsers(@PathVariable("id") Integer id) {
+		return userService.getUser(id);
 	}
 
 	@PostMapping("/user") // create
-	void createUser(@RequestBody User user) {
+	@ResponseStatus(code = HttpStatus.CREATED)
+	void createUser(@RequestBody @Valid User user) {
 		System.out.println(user.getName());
 		userService.saveUser(user);
 	}
--- src/main/java/com/example/demo/service/IUserService.java
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,12 @@ public class IUserService {
 	UserRepository userRepository;
 	public void saveUser(User user) {
 		userRepository.save(user);
+	}
+	public Iterable<User> getUsers() {
+		return userRepository.findAll();
+	}
+	public Optional<User> getUser(Integer id) {
+		return userRepository.findById(id);
 	}
 
 }
